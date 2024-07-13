bd9a272810a1c0a9de937d76dc833a76624fca3a
Lucas Andrade <lucas.andrade@n1.ag>
2024-07-13 08:14:22 -0300

    feat: copyright

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,6 +1,8 @@
 import { Newsletter } from "site/components/Footer/Newsletter.tsx";
 import { Nav } from "site/components/Header/index.tsx";
 import { SocialMediaLinks } from "./SocialMediaLinks.tsx";
+import Image from "apps/website/components/Image.tsx";
+import { ImageWidget } from "apps/admin/widgets.ts";
 
 export interface SocialMedia {
   facebook?: {
@@ -25,9 +27,10 @@ export interface Props {
   title?: string;
   menu: Nav[];
   socialmedia?: SocialMedia;
+  logo: ImageWidget;
 }
 
-export default function Footer({ title, menu, socialmedia }: Props) {
+export default function Footer({ title, menu, socialmedia, logo }: Props) {
   return (
     <div class="w-full flex justify-center px-5 pt-20 pb-10 bg-[#FCEED5] rounded-t-3xl">
       <div class="w-full max-w-[1180px] flex flex-col gap-10">
@@ -46,6 +49,29 @@ export default function Footer({ title, menu, socialmedia }: Props) {
             instagram={socialmedia?.instagram}
             facebook={socialmedia?.facebook}
           />
+        </div>
+        <div class="w-full h-0.5 bg-[#CCD1D2] "></div>
+        <div class="w-full flex items-center justify-between">
+          <p class="text-base text-[#667479] font-semibold">
+            © 2024 Monito. All rights reserved.
+          </p>
+          <Image src={logo} alt="Logo" width={115} />
+          <div class="flex items-center gap-10">
+            <a
+              href="/"
+              class="text-base text-[#667479] font-semibold hover:text-[#5b6366]"
+              target="_blank"
+            >
+              Terms of Service
+            </a>
+            <a
+              href="/"
+              class="text-base text-[#667479] font-semibold hover:text-[#5b6366]"
+              target="_blank"
+            >
+              Privacy Policy
+            </a>
+          </div>
         </div>
       </div>
     </div>
